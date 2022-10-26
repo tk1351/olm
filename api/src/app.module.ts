91fb66ac94395f27bc4be57f3ce9459a083419b7
tk1351 <takakikitsui0111@gmail.com>
2022-10-27 08:42:07 +0900

    feat: connect db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmConfigService } from './config/typeorm.config';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    MoviesModule,
+  ],
   controllers: [],
   providers: [],
 })
